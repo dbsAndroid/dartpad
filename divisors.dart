2f47d07243bda60cfbc3273b877defc1daf83a89
dbsAndroid <10353856@mydbs.ie>
2017-10-05 07:38:27 +0100

    cleaning

--- divisors.dart
+++ divisors.dart
@@ -1,5 +1,15 @@
+/**
+ * “You are in a hotel and you forgot what room number you are in. You remember that the sum
+ *  of its divisors (excluding the number itself) is greater than the number, yet there is
+ *  no subset of those divisors that add up to the number itself. There are 100 rooms. What
+ * is your room number?"
+ */
+
 List divisors = [];
 int divSum = 0;
+int divSumEx = 0;
+int s = 1;
+int t = 0;
 
 void main() { 
   for (int s = 1; s <= 100; s++){
@@ -19,11 +29,18 @@ void main() {
   for (final x in divisors) {
     divSum += x;
   }
+  int divSumEx = divSum - s;
   print('sum of divisors is now ' + divSum.toString());
   print('sum of divisors (excluding number itself) is now ' +
       (divSum - s).toString());
+  if (divSumEx > s){
+       t += 1;
+  }
+  print('t, the number of divSumEx > s = '+t.toString());
   divSum = 0;
   divisors = [];
-  
   }
+
+  //outside loop
+  print('t, the number of divSumEx > s = '+t.toString());
 }
