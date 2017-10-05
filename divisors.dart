0e1cbfceef6510b782cf8eea756554aa1225cbb3
dbsAndroid <10353856@mydbs.ie>
2017-10-05 19:46:26 +0100

    Answer among 22nums

--- divisors.dart
+++ divisors.dart
@@ -3,20 +3,27 @@
  *  of its divisors (excluding the number itself) is greater than the number, yet there is
  *  no subset of those divisors that add up to the number itself. There are 100 rooms. What
  * is your room number?"
+ *
+ * To run this app: dart divisors.dart
+ *
+ * github link is 'https://github.com/dbsAndroid/dartpad/blob/master/divisors.dart'
+ *
+ * cf 'https://en.wikipedia.org/wiki/Divisor_function'
+ * 2017/10/05
  */
-
-import 'dart:convert';     //needed later?
 
 List divisors = [];
 List divRange = [];
+int rooms = 100;
 int divSum = 0;
 int divSumEx = 0;
 int s = 1;
 int t = 0;
-Map divRangeMap = new Map();
+List sumExNums = [];
+//Map divRangeMap = new Map();   //use later to find subsets of Lists and possible sums.
 
 void main() { 
-  for (int s = 1; s <= 100; s++){
+  for (int s = 1; s <= rooms; s++){
      print('\ndivisors of ' + s.toString() + ' are:');
   	for (int i = 1; i <= s / 2; i++) {
     	if (s % i == 0) {
@@ -30,6 +37,7 @@ void main() {
   int divLength = (divisors.length);
   print('length of divisors is now ' + divLength.toString());
 
+  //forEach
   for (final x in divisors) {
     divSum += x;
   }
@@ -40,19 +48,47 @@ void main() {
 
   if (divSumEx > s) {
     t += 1;
-    //for (final y in divisors) {
-     // divRangeMap.addAll(K, V);
-      //divRangeMap.putIfAbsent(divisors.insert(s, element), ifAbsent)
-    //}
+    sumExNums.add(s);
+    //create HashMap here to find subgroups of each array?
   }
   print('divisors are now: ' + divisors.toString());
-  print('divisorsRange are now: ' + divRange.toString());
+  //print('divisorsRange are now: ' + divRange.toString());
+  //print('t, the number of divSumEx > s = '+t.toString());
 
-     //print('t, the number of divSumEx > s = '+t.toString());
+  //reset variables for another loop
   divSum = 0;
   divisors = [];
   }
 
-  //outside loop
-  print('\nt, the number of divSumEx > s = '+t.toString());
+  //outside  100 rooms loop
+  print('\nt, the number of divSumEx(divisors sum) > s(loop number of 100 rooms) = '+t.toString());
+  print('these numbers are:');
+  print(sumExNums);
+  print('the solution is, hopefully, one of these above numbers'
+      '\nand ' + (rooms - t).toString() + '% of the field has been cleared.');
 }
+
+/**
+ * Sample Output:
+ * divisors of 100 are:
+ *  1
+ *  2
+ *  4
+ *  5
+ *  10
+ *  20
+ *  25
+ *  50
+ *  100
+ *  [1, 2, 4, 5, 10, 20, 25, 50, 100]
+ *  length of divisors is now 9
+ *  sum of divisors is now 217
+ *  sum of divisors (excluding number itself) is now 117
+ *  divisors are now: [1, 2, 4, 5, 10, 20, 25, 50, 100]
+ *
+ *  t, the number of divSumEx(divisors sum) > s(loop number of 100 rooms) = 22
+ *  these numbers are:
+ *  [12, 18, 20, 24, 30, 36, 40, 42, 48, 54, 56, 60, 66, 70, 72, 78, 80, 84, 88, 90, 96, 100]
+ *  the solution is, hopefully, one of these above numbers
+ *  and 78% of the field has been cleared.
+ */
